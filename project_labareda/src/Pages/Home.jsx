fac39f0379fd8095bb74653bbbd84f5add086b9c
Alyson Pereira <alyson.bosch.senai14@gmail.com>
2025-08-15 23:04:29 -0300

    Upload File 15/08 - Feature: Finishing the home page Singer with HTML, and now we're going to use tailwindCSS to fast style, and create another pages

--- project_labareda/src/Pages/Home.jsx
+++ project_labareda/src/Pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from "react";
-import { personagem } from "./personagens";
 import { Header } from "../Components/Header";
 import { Footer } from "../Components/Footer";
-import ImagemLabareda from "../assets/imagem-musica.jpg";
+import ImagemLabareda01 from "../assets/labareda01.jpg";
+import ImagemLabareda02 from "../assets/labareda02.png";
+
+function Artista(props) {
+  return (
+    <section>
+      <img src={props.img} alt={props.altImg}></img>
+      <h2>{props.nome}</h2>
+      <h3>{props.carreira}</h3>
+      <p>{props.descricao}</p>
+    </section>
+  );
+}
 
 export function Home() {
   return (
@@ -15,7 +26,7 @@ export function Home() {
 
       <section>
         <h2>Labareda</h2>
-        <img src={ImagemLabareda} alt="Imagem da Música 01"></img>
+        <img src={ImagemLabareda01} alt="Imagem da Música 01"></img>
         <p>
           A história da música 'Labareda', que foi interpretada pela dupla
           Jefferson & Suellen, é uma expressão de fé e espiritualidade
@@ -26,23 +37,34 @@ export function Home() {
       </section>
 
       <section>
-        <img src={""} alt="Imagem da Música 02"></img>
+        <img src={ImagemLabareda02} alt="Imagem da Música 02"></img>
         <p>
           O significado da música 'Vem como labareda de fogo' é um pedido para
           que essa mesma energia e poder divinos se manifestam nos dias atuais,
-          tocando os cora ções e as almas dos crentes.
+          tocando os corações e as almas dos crentes. Ela conta sobre um desejo
+          ardente por uma inspiração espiritual intensa e transformadora. Labar
+          eda é uma música que, além de seu religioso significado, busca conect
+          ar os fiéis com uma sensação de comunidade e propósito compartilhado
+          para conectar mais com Deus e ter fé, recebendo a graça divina e ser
+          em transformado pelo 'fogo' do <strong> Espírito Santo </strong>
         </p>
       </section>
 
+      <h2>Artistas Disponíveis</h2>
+
       <section>
         <figure>
-          <img src={""} alt="Primeiro Artista"></img>
-          <figcaption>{personagem.nome}</figcaption>
+          <img src={<Artista img="" />} />
+          <figcaption>
+            <Artista nome="Jefferson" idade="" descricao="" />
+          </figcaption>
         </figure>
 
         <figure>
-          <img src={""} alt="Segundo Artista"></img>
-          <figcaption>{personagem.nome}</figcaption>
+          <img src={<Artista img="" />} />
+          <figcaption>
+            <Artista nome="Suellen" idade="" descricao="" />
+          </figcaption>
         </figure>
       </section>
 
